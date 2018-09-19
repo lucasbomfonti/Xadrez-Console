bdcd0a8d8d6b25973566f28a68561ca3595d7df1
Lucas <lucas.bomfonti@uds.com.br>
2018-09-19 08:51:43 -0300

    Implementando pécas no tabuleiro

--- Xadrez-Console/tabuleiro/Tabuleiro.cs
+++ Xadrez-Console/tabuleiro/Tabuleiro.cs
@@ -16,5 +16,11 @@
         {
             return pecas[linha, coluna];
         }
+
+        public void ColocarPeca(Peca p, Posicao pos)
+        {
+            pecas[pos.linha, pos.coluna] = p;
+            p.posicao = pos;
+        }
     }
 }
